4c8f0f1d406e264bf0bc23cc459577def143bba8
MyVanThan <minhculong2509@gmail.com>
2023-04-17 08:53:35 +0700

    i beg you

--- main.js
+++ main.js
@@ -3,10 +3,14 @@ var screenHeight = window.innerHeight
 const numCountELe = document.querySelector('.introduction__box__counting-number span')
 var animatedFrists = document.querySelectorAll('.header__content .first-trigger')
 var stillFirstCheck = true
+var a = 150;
+var b = 250;
 var companyName = document.querySelector(".company-name")
 
 if (window.innerWidth < 768) {
   companyName.classList.remove("trigger")
+  a = 0;
+  b = 0;
 } else {
 
 }
@@ -33,11 +37,11 @@ function screenCheck() {
   animatedEles.forEach(function(animatedEle){
     var rect = animatedEle.getClientRects()[0]
     var numCountELeRect = numCountELe.getClientRects()[0]
-    if(!(rect.top > screenHeight - 250 || rect.bottom < 150)) {
+    if(!(rect.top > screenHeight - b || rect.bottom < a)) {
       animatedEle.classList.add('start')
     }
     if(stillFirstCheck){
-      if(!(numCountELeRect.top > screenHeight - 150 || numCountELeRect.bottom < 150)) {
+      if(!(numCountELeRect.top > screenHeight - a || numCountELeRect.bottom < a)) {
         handleNumRaisingAnmited()
         stillFirstCheck = false
       } 
